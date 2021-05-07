eda8e95f2c93f6d887a08ab86628a3e8680c9a6e
Kiyo5hi <i@k1yoshi.com>
2021-05-07 23:11:23 +0800

    :bug: Correct the POST URL

--- weather/main.js
+++ weather/main.js
@@ -4,7 +4,7 @@ const axios = require("axios").default;
 (async () => {
     try {
         const res = await sensor.read(11, 14);
-        await axios.post("localhost:3000", {
+        await axios.post("http://localhost:3000/weather", {
             temperature: res.temperature.toFixed(1),
             humidity: res.humidity.toFixed(1)
         })
