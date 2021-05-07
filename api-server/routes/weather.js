7db8697609363aa829c8e763fbb84706b6e1d262
Kiyo5hi <i@k1yoshi.com>
2021-05-07 23:26:49 +0800

    :bug: Fix status code

--- api-server/routes/weather.js
+++ api-server/routes/weather.js
@@ -4,18 +4,18 @@ const WeatherData = require("../models/WeatherData")
 
 router.get('/:date?/:hour?', async (req, res, next) => {
     // TODO: Database interaction
-    const date = req.query["date"] || req.params["date"] || req.app.getDate();
-    const hour = req.query["hour"] || req.params["hour"] || req.app.getHour();
-
     try {
+        const date = req.query["date"] || req.params["date"] || req.app.getDate();
+        const hour = req.query["hour"] || req.params["hour"] || req.app.getHour();
+
         const weather = await WeatherData.findOne({ date, hour});
         if (!weather) throw new Error(`Weather data not existed on ${date} at ${hour}`);
 
-        res.json(weather);
         res.status(200)
+        res.json(weather);
     } catch (err) {
-        res.json({ error: err.message || err });
         res.status(404);
+        res.json({ error: err.message || err });
     }
 
     next();
@@ -24,18 +24,19 @@ router.get('/:date?/:hour?', async (req, res, next) => {
 router.post('/',async (req, res, next) => {
     try {
         const weatherData = await new WeatherData({
-            date: res.body["date"] || res.app.getDate(),
+            date: res.body['date'] || res.app.getDate(),
             hour: res.body['hour'] || res.app.getHour(),
             temperature: res.body['temperature'],
-            humidity: res.body['humidity']
+            humidity: res.body['date']
         });
+
         await weatherData.save();
 
-        res.json(req.body);
         res.status(201);
+        res.json(req.body);
     } catch (err) {
-        res.json({ error: err.message });
         res.status(400);
+        res.json({ error: err.message });
     }
 
     next();
